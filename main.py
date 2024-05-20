75da15e3d58fca5449dfe18789068f6f4156e58c
Andre <andre@andre-Aspire-A315-56>
2024-05-20 15:56:52 -0300

    dfs

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from skimage.filters import threshold_multiotsu
 
 from PIL import Image
 
+from collections import deque
+
 PATH = "solda.png"
 
 def otsu_histogram(im:object, thresholds: tuple):
@@ -16,27 +18,29 @@ def otsu_histogram(im:object, thresholds: tuple):
 
 def binary_threshold(im:object)->object:
     image = Image.fromarray(np.uint8(im)).convert('L')
+
     (l, h) = image.size
 
     out = Image.new(mode='L', size=(l, h))
 
     for j in range(0, h):
         for i in range(0, l):
-            if image.getpixel((i, j)) > 1:
+            if image.getpixel((i, j)) == 0:
                 out.putpixel(
                     (i, j),
-                    1055
+                    0
                 )
             else:
                 out.putpixel(
                     (i, j),
-                    0
+                    255
                 )
 
     return out
 
 def threshold(im:object)->object:
     thresholds = threshold_multiotsu(im)
+
     regions = np.digitize(image, bins=thresholds)
 
     return binary_threshold(regions)
@@ -67,83 +71,106 @@ def NO(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
 
     if im.getpixel((i - 1, j - 1)) == size:
-        return True
-    return False
+        return (i - 1, j - 1)
+    return None
 
-def N(im:object, coordinate:tuple, size:int)->bool:
+def N(im:object, coordinate:tuple, size:int)->tuple:
     (i, j) = coordinate
     
     if im.getpixel((i - 1, j)) == size:
-        return True
-    return False
+        return (i - 1, j)
+    return None
 
 def NE(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i - 1, j + 1)) == size:
-        return True
-    return False
+        return (i - 1, j + 1)
+    return None
 
 def O(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i, j - 1)) == size:
-        return True
-    return False
+        return (i, j - 1)
+    return None
 
 def L(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i, j + 1)) == size:
-        return True
-    return False
+        return (i, j + 1)
+    return None
 
 def SO(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i + 1, j - 1)) == size:
-        return True
-    return False
+        return (i + 1, j - 1)
+    return None
 
 def S(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i + 1, j)) == size:
-        return True
-    return False
+        return (i + 1, j)
+    return None
 
 def SE(im:object, coordinate:tuple, size:int)->bool:
     (i, j) = coordinate
     
     if im.getpixel((i + 1, j + 1)) == size:
-        return True
-    return False
+        return (i + 1, j + 1)
+    return None
 
-def calculate_coordinate(im, coordinate:tuple, size:int)->bool:
+def calculate_coordinate(im, coordinate:tuple, size:int)->list:
     list_cordinate_functions = [NO, N, NE, O, L, SO, S, SE]
     list_pixels = [] # [NO, N, NE, O, L, SO, S, SE]
 
     for fi in list_cordinate_functions:
         list_pixels.append(fi(im, coordinate, size))
 
-    return True in list_pixels
+    return [i for i in list_pixels if i != None]
+
+def dfs(im:object, out:object, coordinate:tuple, components:int)->set:
+    set_coordinates:set = set()
+    queue:deque = deque()
+
+    set_coordinates.add(coordinate)
+    queue.append(coordinate)
+
+    while queue:
+        for i in range(len(queue)):
+            (i, j) = queue.popleft()
+
+            coordinates = calculate_coordinate(im, (i, j), 255)
+
+            if len(coordinates) != 0:
+                out.putpixel((i, j), components)
+                for k in coordinates:
+                    if k not in set_coordinates:
+                        queue.append(k)
+                    set_coordinates.add(k)
+
+    return set_coordinates
 
 def lettering_counting(im:object)->tuple:
     (l, h) = im.size
 
-    list_components = [1]
+    components = 10
+
+    list_components:list = [components]
+    set_coordinates:set = set()
 
     out = Image.new(mode='L', size=(l, h))
 
-    components = 1
-
     for j in range(1, h - 1):
         for i in range(1, l - 1):
-            if calculate_coordinate(im, (i, j), 255):
-                out.putpixel((i, j), 100 + components)
-            elif im.getpixel((i, j)) != 0:
-                components += 1
-                list_components.append(components)
+            if im.getpixel((i, j)) != 0 and (i, j) not in set_coordinates:
+               set_coordinates = set_coordinates.union(dfs(im, out, (i, j), components)) 
+               components += 1
+               list_components.append(components)
+            elif im.getpixel((i, j)) == 0:
                 out.putpixel((i, j), 0)
 
     return (out, list_components)
@@ -189,7 +216,7 @@ def max_component(im:object, list_components:list)->int:
     dict_components = {}
 
     for i in list_components:
-        dict_components[i + 100] = 0
+        dict_components[i] = 0
 
     for j in range(0, h):
         for i in range(0, l):
@@ -223,28 +250,39 @@ def center_of_mass_image(im:object, list_components:list)->tuple:
         
     return (ceil(sum_x/cont_x), ceil(sum_y/cont_y))
 
+def psudo_color(im: object):
+    ...
+    
+def main(file: str):
+    ...
+
 def open_image(im:str, gray:bool = True)->object:
     return imread(im, as_gray=gray)
+
+
 
 if __name__ == "__main__":
     image = open_image(PATH, True)
 
-    image = threshold(image)
+    threshold_image = threshold(image)
 
-    image.save("results/threshold.jpg")
+    threshold_image.save("results/binary_threshold.jpg")
 
-    zero_image = zero_frame(image)
+    zero_image = zero_frame(threshold_image)
 
     zero_image.save("results/zero_image.jpg")
 
     (components_image, list_components) = lettering_counting(zero_image)
+
+    components_image.save("results/components_threshold.jpg")
     
     list_components_side = list(identifica_side(components_image))
+    # print(list_components_side)
 
     remove_side_component_image = remove_side_component(components_image, list_components_side)
     remove_side_component_image.save("results/remove_side_component_image.jpg")
 
-    print(max_component(remove_side_component_image, list_components))
+    # print(max_component(remove_side_component_image, list_components))
 
     (i, j) = center_of_mass_image(remove_side_component_image, list_components)
     print(i, j)
